7b3078a31baf7f4f4ad8d580a4ddd31db3b594f2
CounKite <43300000+CounKite@users.noreply.github.com>
2018-10-04 19:54:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+                                                                                
+                                                                                                                                 
+bot.login('NDk1NTUwMDY1OTk0MjM1OTA0.DpDsew.zux_zbf8JFvxcqxkx3Mrn165foM');
+
